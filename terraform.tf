0d2032e5688d7e2e158839220f61d4c09fb92937
Gene Laisne <glaisne@users.noreply.github.com>
2025-06-13 23:05:35 -0400

    Initial commit

---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_version = ">=1.7.2"
+
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 4.30.0"
+        }
+    }
+    backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+  # Optional: reference variables if you truly need to pin a sub/tenant
+  # subscription_id = var.subscription_id
+  # tenant_id       = var.tenant_id
+}
